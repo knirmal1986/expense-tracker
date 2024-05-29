8ba3a694783ec11f21eebaefe3004cb5ab2f9f12
Lakshmi narayana <lnrabc@gmail.com>
2024-05-29 08:00:55 +0530

    get user details

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { database } from 'ngx-bootstrap-icons';
+import { Observable,map } from 'rxjs';
 import { AuthFirebaseService } from 'src/services/auth-firebase.service';
+import { DatabaseService } from 'src/services/database.service';
 
 @Component({
   selector: 'app-home',
@@ -8,17 +11,30 @@ import { AuthFirebaseService } from 'src/services/auth-firebase.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit{
-  
+  loggedInUserDetails:any
   private authService:AuthFirebaseService
-
+  private dbService:DatabaseService
   constructor(private router:Router){
     this.authService = inject(AuthFirebaseService)
+    this.dbService=inject(DatabaseService)
   }
   
   ngOnInit(): void {
     if(!this.authService.checkIfLoggedIn()){
       alert("Please login")
       this.router.navigate(['/login'])
+    }else{
+      this.dbService.getAll().snapshotChanges().pipe(
+        map(changes =>
+          changes.map(c =>
+            ({ id: c.payload.doc.id, ...c.payload.doc.data() })
+          )
+        )
+      ).subscribe(data =>{
+        this.loggedInUserDetails = data.filter(function (record){
+          return record.UID == window.localStorage.getItem("username")
+        })
+      })
     }
 }
 }
